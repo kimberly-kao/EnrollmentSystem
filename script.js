48efd6c84e87f812f43743b62fdf5b058e10654f
kimberly-kao <kaofamilyca@gmail.com>
2021-04-28 15:02:12 -0700

    updated buttons

---
+++ script.js
@@ -0,0 +1,5 @@
+function studentDecision() {
+  window.location.href = 'overview.html' 
+  studentView.getElementById("acceptStu").classList.remove("hidden");
+  studentView.getElementById("rejectStu").classList.remove("hidden");
+}
