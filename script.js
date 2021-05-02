3bca1ab5351218221e2e64fd737a3a5220b01377
kimberly-kao <kaofamilyca@gmail.com>
2021-05-02 15:25:18 -0700

    trying to update css of login

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function studentDecision() {
   window.location.href = 'profiles.html' 
-  studentView.getElementById("acceptStu").classList.remove("hidden");
-  studentView.getElementById("rejectStu").classList.remove("hidden");
+  studentview.getElementById("acceptStu").classList.remove("hidden");
+  studentview.getElementById("rejectStu").classList.remove("hidden");
 }
