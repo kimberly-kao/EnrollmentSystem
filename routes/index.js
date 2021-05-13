8dd29b53d00fe199d5c035dfc707b0d30bfbf546
Nicole C <nicas@sonic.net>
2021-05-13 05:00:01 -0700

    Everything on the website works except for waitlisting/acepting etc buttons and adding a new student

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-var InfoDump = require('./infoDump')
 var Student = require('./student')
 var Officer = require('./admissions')
 var OfficerChanges = require('./officerChanges')
 
-router.use("/infoDump", InfoDump)
 router.use("/student", Student)
 router.use("/admissions", Officer)
 router.use("/changes", OfficerChanges)
